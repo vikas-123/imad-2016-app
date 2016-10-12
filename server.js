5c528827c6e3408d4c3a4540b66c940b5cfdd242
vikas-123 <1995vikasgupta@gmail.com>
2016-10-12 16:09:26 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one', function (req, res) {
-  res.send('artcle one is requested and served');
+  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 
 app.get('/article-two', function (req, res) {
